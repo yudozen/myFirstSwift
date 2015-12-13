5ce74bce848e28781ac408ffef1c173c8c1c52e8
Yuki Nakamura <nakamura_yuki@kinako4122.local>
2015-12-13 22:16:52 +0900

    add sample fun

--- calculater/ViewController.swift
+++ calculater/ViewController.swift
@@ -9,7 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var screen: UILabel!
+    var firstNumber = Int()
+    var secondNumber = Int()
+    var isTypingNumber = false
+    var result = Int()
+    var operation = ""
 
+    @IBAction func number(sender: AnyObject) {
+        let number = sender.currentTitle
+        if isTypingNumber == true {
+            screen.text = screen.text! + number!!
+        } else {
+            screen.text = number;
+        }
+        isTypingNumber = true
+        
+    }
+    
+    @IBAction func operation(sender: AnyObject) {
+        isTypingNumber = false
+        firstNumber = Int(screen.text!)!
+        operation = sender.currentTitle!!
+    }
+    
+    @IBAction func equel(sender: AnyObject) {
+        secondNumber = Int(screen.text!)!
+        if operation == "+" {
+            result = firstNumber + secondNumber
+        } else if operation == "-" {
+            result = firstNumber - secondNumber
+        } else if operation == "*" {
+            result = firstNumber * secondNumber
+        } else {
+            result = firstNumber / secondNumber
+        }
+        screen.text = "\(result)"
+    }
+    
+    @IBAction func clear(sender: AnyObject) {
+        firstNumber = 0
+        secondNumber = 0
+        isTypingNumber = false
+        result = 0
+        screen.text = "\(result)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
